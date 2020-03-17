c9736e757c826cf91ee90b9a71a9763f2c54dbcf
Stephen Reed <domotang@hotmail.com>
2020-03-17 17:00:23 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# progress-timeline-bar
+
+Highly customizable animated progress timeline bar for React which emphasizes due proccess
+
+![screen cap of demo](https://raw.githubusercontent.com/domotang/progress-timeline-bar/master/ptb.jpg)
+![screen cap of demo](https://raw.githubusercontent.com/domotang/progress-timeline-bar/master/ptb_det.jpg)
+
+current demo
+http://domosavant.com/
+
+2.24.2020 - Adding a 'card' mode in order to utilize in grid environments, handy for showing recipes, etc.\
+1.11.2020 - Added an event scroller. This solves two issues: (1) Quantity of events no longer limited to bar width. (2) Mobile friendly.
+
+Currently in development. API documentation pending release\
+Utilizes all SVG graphics/animations\
+Utilizes GreenSock library. Otherwise vanilla js\
+Designed to be a consumable React component\
+Modular template system for look and feel customizations\
+Data is mock garbage modeling a returned list of shipments
+
+todo\
+*make responsive\
+*create five skin plugins\
+\*publish full api documantation
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+var debug = process.env.NODE_ENV !== "production";
+const path = require("path");
+var Visualizer = require("webpack-visualizer-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const htmlWebpackPlugin = new HtmlWebpackPlugin({
+  template: path.join(__dirname, "src/index.html")
+});
+console.log(process.env.NODE_ENV);
+module.exports = {
+  // performance: {
+  //   hints: "warning",
+  //   maxEntrypointSize: 212000,
+  //   maxAssetSize: 212000
+  // },
+  // entry: path.join(__dirname, "src/index.js"),
+  entry: {
+    index: path.join(__dirname, "src/index.js"),
+    //cont: path.join(__dirname, "src/lib/PTBController.js"),
+    //template: path.join(__dirname, "src/template/PTBTemplateMaterial.js"),
+    //animations: path.join(__dirname, "src/template/pTBMaterialAnimations.js"),
+    // tmpComponents: path.join(
+    //   __dirname,
+    //   "src/template/pTBMaterialComponents.js"
+    // ),
+    globe: path.join(__dirname, "src/lib/ne_110m_land.json")
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    // filename: "[name]bundle.js"
+    // filename: "bundle.js"
+    chunkFilename: "[name].bundle.js"
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all"
+    }
+  },
+  devtool: debug ? "inline-source-map" : false,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg|jpg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  // plugins: [htmlWebpackPlugin, new Visualizer()],
+  plugins: [htmlWebpackPlugin],
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    inline: true,
+    writeToDisk: false,
+    hot: true
+  }
+};
